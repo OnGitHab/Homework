bca3df13cf86098cf01afe9827e1b33e27bd1ca7
user <youngerig@gmail.com>
2024-06-12 06:09:09 -0700

    Сhanged code structure

--- 5.1.py
+++ 5.1.py
@@ -1,23 +1,24 @@
-import keyword
 import string
 
 st_nm = "1234567890"
 st_st = string.punctuation.replace("_", " ")
-lst_kw = keyword.kwlist
+lst_kw = ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield', "get"]
 
 user_input = input("...:\n")
 
 
-if (user_input[0] in st_nm) == True:
-    print("Variable cannot begin with a digit")
-elif (user_input.istitle()) == True:
-    print("Variable cannot contain capital letters")
-elif (user_input in st_st) == True:
-    print("Variable cannot contain spaces, and punctuation")
-elif (user_input in lst_kw) == True:
-        print("Variable cannot be any of the keywords")
+if user_input[0] in st_nm:
+    print("False")
+elif user_input:
+    print(user_input.istitle())
+elif user_input in st_st or "__" in user_input:
+    print("False")
+elif len(user_input) > 0:
+    for el in lst_kw:
+        common_chars = set(user_input) & set(el)
+        similarity = len(common_chars) / max(len(set(user_input)), len(set(el)))
+        if similarity == 1:
+            print("False")
 else:
-    print("Can be variable")
-
-
+    print("True")
 
