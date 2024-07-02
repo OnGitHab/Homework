49778d131845ab736388492e36a5f0b6ff34c620
user <youngerig@gmail.com>
2024-07-02 10:29:36 -0700

    Added homework 11.1 - 11.3

--- 11.2.py
+++ 11.2.py
@@ -21,3 +21,4 @@ assert list(generate_cube_numbers(10)) == [8]
 assert list(generate_cube_numbers(100)) == [8, 27, 64]
 assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000]
 print("OK")
+
