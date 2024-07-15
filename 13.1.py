fd3d9e53ff10320d414bbaab2d351fef32509c31
user <youngerig@gmail.com>
2024-07-15 04:39:16 -0700

    Added class Person

--- 13.1.py
+++ 13.1.py
@@ -1,4 +1,13 @@
-13.1
+class Person:
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        person_info = f"Gender:{self.gender}, Age: {self.age}, Name: {self.first_name}, {self.last_name}"
+        return person_info
 
 
 class Student(Person):
