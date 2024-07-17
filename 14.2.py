d392642242c69ca246a02484eaa40f105b403161
user <youngerig@gmail.com>
2024-07-17 06:43:33 -0700

    14.2

--- 14.2.py
+++ 14.2.py
@@ -1,15 +1,19 @@
-from Group_modul import Student, Group
+from Group_package import class_Student
+from Group_package import class_Group
 
-st1 = Student("Male", 30, "Steve", "Jobs", "AN142")
-st2 = Student("Female", 25, "Liza", "Taylor", "AN145")
-gr = Group("PD1")
+st1 = class_Student.Student("Male", 30, "Steve", "Jobs", "AN142")
+st2 = class_Student.Student("Female", 25, "Liza", "Taylor", "AN145")
+gr = class_Group.Group("PD1")
 gr.add_student(st1)
 gr.add_student(st2)
 
 
 assert str(gr.find_student("Jobs")) == str(st1)
 assert gr.find_student("Jobs2") is None
-assert isinstance(gr.find_student("Jobs"), Student) is True
+assert isinstance(gr.find_student("Jobs"), class_Student.Student) is True
 
 gr.delete_student("Taylor")
+print(gr.number)
+
 print(gr)
+
